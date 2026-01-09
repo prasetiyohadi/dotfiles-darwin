69b6502b564d187807a8884097aed6f14ed6ba22
Prasetiyo Hadi Purwoko <prasetiyohadi92@gmail.com>
2026-01-09 10:19:30 +0700

    update: add comments for sync.sh

--- sync.sh
+++ sync.sh
@@ -2,25 +2,35 @@
 set -euo pipefail
 
 # Prepare config directories
+echo '### Preparing config directories...' | gum format
 mkdir -p ~/.{config,nap,ssh}
 mkdir -p ~/.config/{atuin,ghostty,mise,pet}
 
-# Generate config file from template
+# Generate config files from template
+echo '### Generating config files from template...' | gum format
 envsubst <.config/pet/config.toml.tpl >.config/pet/config.toml
 envsubst '$GIT_EMAIL,$GIT_NAME' <.gitconfig.tpl >.gitconfig
 
+# Secure config files containing secrets
+echo '### Securing config files containing secrets...' | gum format
 chmod og-rwx .config/pet/config.toml
 
-# CURRENT_DATE=$(date +%Y%m%d-%H%M%S)
-# mv -v ~/.config/atuin/config.toml{,".bak-$CURRENT_DATE"}
+# Create backup for existing config files
+echo '### Creating backup for the existing config files...' | gum format
+CURRENT_DATE=$(date +%Y%m%d-%H%M%S)
+# mv -v ~/.config/atuin/config.toml{,".bak-$CURRENT_DATE"} # atuin auto-generated its config upon install so we need to backup it first, uncomment this line on initial sync
 # mv -v ~/.config/mise/config.toml{,".bak-$CURRENT_DATE"}
 # mv -v ~/.config/ghostty/config{,".bak-$CURRENT_DATE"}
 # mv -v ~/.config/pet/config.toml{,".bak-$CURRENT_DATE"}
+# mv -v ~/.config/starship.toml{,".bak-$CURRENT_DATE"}
+# mv -v ~/.nap/config.yaml{,".bak-$CURRENT_DATE"}
 # mv -v ~/.gitconfig{,".bak-$CURRENT_DATE"}
 # mv -v ~/.tmux.conf{,".bak-$CURRENT_DATE"}
+# mv -v ~/.zprofile{,".bak-$CURRENT_DATE"}
 # mv -v ~/.zshrc{,".bak-$CURRENT_DATE"}
 
 # Create symlinks for the config files
+echo '### Creating symlinks for the config files...' | gum format
 stow .
 
-echo '### Execute `source ~/.zshrc`.' | gum format
+echo '### TODO: Execute `source ~/.zshrc`' | gum format
